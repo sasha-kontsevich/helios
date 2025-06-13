75358ee03bae18392a9ea38264243dceccd10098
kontsevich <sasha.kontsevich@gmail.com>
2025-06-13 11:39:29 +0300

    camera rotaaaaaatiiiiiiii

--- examples/astris/src/systems/CameraRotationSystem.ts
+++ examples/astris/src/systems/CameraRotationSystem.ts
@@ -1,4 +1,4 @@
-import {Context, Position, System} from "@merlinn/helios-core";
+import {Position, System} from "@merlinn/helios-core";
 import { defineQuery } from "bitecs";
 import {ThreeCamera} from "../components";
 
@@ -6,18 +6,15 @@ export class CameraRotationSystem extends System {
     private query = defineQuery([ThreeCamera, Position]);
     private radius = 5;
     private time = 0;
-    private speed = -0.1
+    private speed = -0.2
 
     update(deltaTime: number): void {
         this.time += deltaTime;
 
         this.query(this.world).forEach((eid) => {
-            Position.x[eid] = Math.sinh(this.time * this.speed) * this.radius;
+            Position.x[eid] = Math.sin(this.time * this.speed) * this.radius;
             Position.z[eid] = Math.cos(this.time * this.speed) * this.radius;
         });
     }
 
-    private getFps(deltaTime: number) {
-        return (1/deltaTime);
-    }
 }
